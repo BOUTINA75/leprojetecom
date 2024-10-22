ef87be2ca3432ae459171973f7ee5f04605d0893
BOUTINA75 <boutina.mihoubi@hotmail.fr>
2024-10-22 13:06:27 +0200

    Les membre

---
+++ src/Controller/LoginController.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Controller;
+
+use Exception;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
+
+class LoginController extends AbstractController
+{
+    #[Route('/connexion', name: 'app_login')]
+    public function index(AuthenticationUtils $authenticationUtils): Response
+    {
+
+
+        // Gérer les erreeurs
+        $error = $authenticationUtils->getLastAuthenticationError();
+
+        // Dernier username (email)
+        $lastUsername = $authenticationUtils->getLastUsername();
+
+
+        return $this->render('login/index.html.twig', [
+            'error' => $error,
+            'last_username' => $lastUsername,
+        ]);
+    }
+
+    #[Route('/deconnexion', name: 'app_logout', methods: ['GET'])]
+    public function logout(): never 
+    {
+        throw new Exception('Don\'t forget to activate logout in security.yaml');
+    }
+}
